26035c3864f0ccc3b202bd26cff70452384d973c
MARTIN-tibo <thibault.m210@gmail.com>
2025-07-07 13:36:36 +0200

    Update menu images

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -6,7 +6,7 @@ const Menu = () => {
     {
       category: 'Entrées',
       image:
-        'https://images.pexels.com/photos/2147491/pexels-photo-2147491.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        'https://i.postimg.cc/QtzxxHdC/Asperges-sarrasin.jpg',
       items: [
         { name: 'Œuf meurette revisité', price: '7€' },
         { name: 'Asperges blanches, cresson, agrumes', price: '9€' },
@@ -21,7 +21,7 @@ const Menu = () => {
     {
       category: 'Plats',
       image:
-        'https://images.pexels.com/photos/3296275/pexels-photo-3296275.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        'https://i.postimg.cc/6qG67t80/ballotine-de-poulet.jpg',
       items: [
         { name: 'Ballotine de poulet basse température', price: '21€' },
         {
@@ -35,7 +35,7 @@ const Menu = () => {
     {
       category: 'Desserts',
       image:
-        'https://images.pexels.com/photos/1126359/pexels-photo-1126359.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        'https://i.postimg.cc/s1dj7qSR/Millefeuille-vanille-pe-can.jpg',
       items: [
         { name: 'Crème brulée au sésame noir', price: '8€' },
         { name: 'Mille-feuilles vanille et noix de pécan', price: '12€' },
